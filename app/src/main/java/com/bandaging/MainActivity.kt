026fe934f6d6294401b5bcacdaca82b95822d416
Ananda Santra <kunalkumar.poddar@webskitters.in>
2024-06-18 16:18:49 +0530

    "changes splashscreen api"

--- app/src/main/java/com/bandaging/MainActivity.kt
+++ app/src/main/java/com/bandaging/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bandaging
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.LaunchedEffect
@@ -9,18 +11,28 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.bandaging.screens.NoInternetAnimatedDialog
 import com.bandaging.screens.NoInternetScreen
 import com.bandaging.screens.WebViewScreen
 import com.bandaging.ui.theme.BandagingTheme
 import com.bandaging.utils.NetworkObserver
 import com.bandaging.utils.Status
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     private lateinit var networkObserver: NetworkObserver
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        installSplashScreen().setKeepOnScreenCondition {
+            runBlocking {
+                delay(700)
+                false
+            }
+        }
         super.onCreate(savedInstanceState)
         networkObserver = NetworkObserver(applicationContext)
         setContent {
@@ -41,7 +53,8 @@ class MainActivity : ComponentActivity() {
                     if (state.value != Status.AVAILABLE) isWebViewScreen = false
                 }
                 if (!isWebViewScreen) NoInternetScreen {
-                    if (state.value == Status.AVAILABLE) isWebViewScreen = true else isShowDialog = true
+                    if (state.value == Status.AVAILABLE) isWebViewScreen = true else isShowDialog =
+                        true
                 } else WebViewScreen()
 
                 NoInternetAnimatedDialog(visible = isShowDialog) {
